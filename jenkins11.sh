0184fcd70a16c39fc5c4d202378dbe3f3e4a01ac
hemachaitanya <b.hemalatha203@gmail.com>
2023-04-27 16:58:10 +0530

    basic shell scripting

---
+++ jenkins11.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+apt-get update
+apt install openjdk-11-jdk -y
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \/usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \ https://pkg.jenkins.io/debian binary/ | sudo tee \ /etc/apt/sources.list.d/jenkins.list > /dev/null
+apt-get update
+apt-get install jenkins -y
+systemctl start jenkins.service
+systemctl status jenkins
