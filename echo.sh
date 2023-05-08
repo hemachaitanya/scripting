e4d57e502d44adbece8de0a0d1bb599fbf8b233b
hemachaitanya <b.hemalatha203@gmail.com>
2023-05-08 11:15:53 +0530

    simple intrest sucess

--- echo.sh
+++ echo.sh
@@ -10,3 +10,32 @@ for i in {1..50}
 do
 echo $i
 done
+
+# print + , * , - , /
+#!/bin/bash
+read X
+read Y
+echo $(( X+Y ))
+echo $(( X-Y ))
+echo $(( X*Y ))
+echo $(( X/Y )) 
+
+# <,>,=
+read x
+read y
+if [ $x -gt $y  ]
+then
+{
+    echo "X is greater than Y  "
+}
+elif [ $x -eq $y ]
+then
+{
+    echo "X is equal to Y"
+}
+else
+{
+  echo "X is less than Y " 
+}
+fi
+
--- studentgrade.sh
+++ studentgrade.sh
@@ -11,16 +11,16 @@ echo "social marks: "
 read s
 echo "gk marks: "
 read g
-percentage=$(echo "($t + $e + $m + $h + $s + $g)/6")
+percentage=$(echo "($t + $e + $m + $h + $s + $g)/6" )
 echo "the percentage of marks is: $percentage"
 
 echo "the percentage marks is : $percentage"
 if [ $percentage -gt 85 ]
 then 
-     echo Grade is 'A'
+     echo "Grade is 'A'"
 elif [ $percentage -lt 85 ]
 then
-     echo Grade is 'B'
+     echo "Grade is 'B'"
 fi
 
 
@@ -43,10 +43,7 @@ echo "social marks: "
 read s
 echo "gk marks: "
 read g
-#percentage=$(echo "($t + $e + $m + $h + $s + $g)/6")
 total=$(($t + $e + $m + $h + $s + $g))
-echo "the total of marks is: $total"
-percentage=$($total / 6)
 echo "Percentage is: $percentage"
 if [ $percentage -gt 80 ]
 then  "echo good"
