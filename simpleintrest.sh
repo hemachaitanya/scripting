0a35c443e5e5eef08401887509dba615f7a74b49
hemachaitanya <b.hemalatha203@gmail.com>
2023-05-08 10:17:21 +0530

    simple intrest sucess

--- simpleintrest.sh
+++ simpleintrest.sh
@@ -1,26 +1,9 @@
 #!/bin/bash
-echo "telugu marks: "
+echo "princile amount is: "
+read p
+echo "time is: "
 read t
-echo "english marks: "
-read e
-echo "maths marks: "
-read m
-echo "hindi marks: "
-read h
-echo "social marks: "
-read s
-echo "gk marks: "
-read g
-#percentage=$(echo "($t + $e + $m + $h + $s + $g)/6")
-total=$(($t + $e + $m + $h + $s + $g))
-echo "the total of marks is: $total"
-percentage=$($total / 6)
-echo "Percentage is: $percentage"
-if [ $percentage -gt 80 ]
-then  "echo good"
-elif [ $percentage -lt 80]
-then "echo bad"
-fi
-
-
-     
+echo "rate of intrest is: "
+read r
+intrest=$(( $p * $t * $r/100 ))
+echo "simple intrest is: $intrest"
--- studentgrade.sh
+++ studentgrade.sh
@@ -22,3 +22,37 @@ elif [ $percentage -lt 85 ]
 then
      echo Grade is 'B'
 fi
+
+
+
+
+
+
+
+
+#!/bin/bash
+echo "telugu marks: "
+read t
+echo "english marks: "
+read e
+echo "maths marks: "
+read m
+echo "hindi marks: "
+read h
+echo "social marks: "
+read s
+echo "gk marks: "
+read g
+#percentage=$(echo "($t + $e + $m + $h + $s + $g)/6")
+total=$(($t + $e + $m + $h + $s + $g))
+echo "the total of marks is: $total"
+percentage=$($total / 6)
+echo "Percentage is: $percentage"
+if [ $percentage -gt 80 ]
+then  "echo good"
+elif [ $percentage -lt 80]
+then "echo bad"
+fi
+
+
+     
